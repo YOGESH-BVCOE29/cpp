5b7801daa68095a80ac87171350d57143808daef
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-16 11:29:19 +0530

    1st pattern

--- patterns.cpp
+++ patterns.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
+   // (1)  square pattern ; easy with numbers n=4
    int n;
-   cout << "enter the number:";
+   cout << "enter the number :";
    cin >> n;
-   
+   for(int i=1; i<=n; i++){
+    for(int j=1; j<=n; j++){
+        cout << j ;
+    } cout << endl;
+   } 
+   // (2)  
+
+
     return 0;
 } 
