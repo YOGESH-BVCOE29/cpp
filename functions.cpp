bc25df38e961fbd3d971fd869b32ac4744f7f216
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-22 14:44:35 +0530

    kk

--- functions.cpp
+++ functions.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 using namespace std;
+
+
+int sum(int a, int b) {
+    int s = a + b;
+    return s;}
+// min of two number .
+int mintwo(int a, int b) {
+    if(a<b){ 
+        return a;
+    } else{
+        return a;
+    }
+}
+
 int main(){
-
-
+  cout << sum(6, 3) << endl;
+ cout << mintwo(5, 6);
 
 
 
