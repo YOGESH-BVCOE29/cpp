f9141bbe9555997ffed4950db5f7c8547294dfab
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-22 20:43:17 +0530

    cc

--- functions2.cpp
+++ functions2.cpp
@@ -7,25 +7,37 @@ cout << "x=" << x << endl;
  
 }
 // calc the sum of digits of a number 
-int sum(int n){
+int sum(int m){
    int digsum =0;
-   while(n > 0){
-    int lastdigit = n%10;
-    n = n/10;
+   while(m > 0){
+    int lastdigit = m%10;
+    m = m/10;
     digsum += lastdigit;
    }
   return digsum;
 }
-
-
+// calculate ncr binomial coefficient of n and r value 
+int fact(int n){
+    int f = 1;
+    for(int i=1; i<=n; i++){
+        f = f*i;
+    } 
+    return f;
+}
+int nCr(int n, int r){
+    int fn = fact(n);
+    int fr = fact(r);
+    int fnr = fact(n-r);
+    return fn / (fr * fnr);
+}
 
 
 int main(){
-
 fun();
 cout << endl;
-
 cout << sum(143)<< endl;
+int n=8, r=2;
+cout << nCr(n, r) << endl;
 
 
     return 0;
