16058ed1e3b9ac665d529b1b7b9b1b7bffbfb8f4
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-22 20:10:37 +0530

    d

--- functions2.cpp
+++ functions2.cpp
@@ -4,7 +4,17 @@ using namespace std;
 void fun(){
 int x = 25;
 cout << "x=" << x << endl;
-
+ 
+}
+// calc the sum of digits of a number 
+int sum(int n){
+   int digsum =0;
+   while(n > 0){
+    int lastdigit = n%10;
+    n = n/10;
+    digsum += lastdigit;
+   }
+  return digsum;
 }
 
 
@@ -13,8 +23,9 @@ cout << "x=" << x << endl;
 int main(){
 
 fun();
+cout << endl;
 
-
+cout << sum(143)<< endl;
 
 
     return 0;
