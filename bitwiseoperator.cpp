830d09b0cca6ad62fd17d333e65a052cc64a80cf
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-22 23:35:58 +0530

    c

--- bitwiseoperator.cpp
+++ bitwiseoperator.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
 
  int a = 4, b=8;
-cout << (a&b) << endl;
-cout << (a | b) << endl;
-
+cout << (a&b) << endl; // bitwise and  = dono condition true honi chiaye 
+cout << (a | b) << endl; // bitwise or = koi bhi ek condition true honiu chaiye
+cout << (a^b) << endl; // bitwise XOR = dono ulte ho tabhi true answer ayega 
 
 
 
