70c822b958adf6fc9ed10134c0ae08a593aae67d
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-22 12:26:28 +0530

    ,

--- patterns2.cpp
+++ patterns2.cpp
@@ -61,11 +61,11 @@ for(int i=0; i<n; i++){
    cout << endl;
 }
 for(int i=0; i<n-1; i++){
-    for(int j=0; j<i; j++){
+    for(int j=0; j<i+1; j++){
         cout << " ";
     } cout << "*";
-    if(i!=0){
-        for(int j=0; j<n-i-1; j++){
+    if(i != n-2){
+        for(int j=0; j<2*(n-i)-5; j++){
             cout << " ";
         } cout << "*";
     }
