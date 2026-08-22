2c8ee0bbab9cb91e07e45d371c2ba3f5c1b82c66
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-22 15:01:27 +0530

    n

--- functions.cpp
+++ functions.cpp
@@ -10,14 +10,30 @@ int mintwo(int a, int b) {
     if(a<b){ 
         return a;
     } else{
-        return a;
+        return b;
     }
+}
+// calc sum of numbers from 1 to n  
+void sumn(int n){
+    int s =0;
+    for(int i=0; i<=n; i++){
+     s = s+i;
+
+    } cout << s;
+}
+// calculating the n factorial 
+int fact(int m){
+    int f = 1;
+    for(int i=0; i<=m; i++){
+        f = f*i;
+    } return f;
 }
 
 int main(){
   cout << sum(6, 3) << endl;
- cout << mintwo(5, 6);
-
+ cout << mintwo(5, 6) << endl; // argument are value passed to a functio
+ sumn(5);
+cout << fact(5) << endl;
 
 
 
