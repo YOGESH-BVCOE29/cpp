62ab39d699f7962d00702ee1f4727e52a5e5deec
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-22 23:50:08 +0530

    ss

--- bitwiseoperator.cpp
+++ bitwiseoperator.cpp
@@ -6,7 +6,8 @@ int main(){
 cout << (a&b) << endl; // bitwise and  = dono condition true honi chiaye 
 cout << (a | b) << endl; // bitwise or = koi bhi ek condition true honiu chaiye
 cout << (a^b) << endl; // bitwise XOR = dono ulte ho tabhi true answer ayega 
-
+cout << (10 << 2) <<endl; // bitwise left shift = isme jo 10 ke binary digits wo do digits age shift ho jayegi aur naya number bana lengi 
+cout << (10 >> 1) << endl; // bitwise right shift
 
 
     return 0;
