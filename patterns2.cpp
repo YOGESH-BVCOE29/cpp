38576b257fde185c13b4663c183b8899c94aa579
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-22 12:06:28 +0530

    nn

--- patterns2.cpp
+++ patterns2.cpp
@@ -44,8 +44,25 @@ for(int i=0; i<n; i++){
 //   *     * 
 //     *  *
 //       *
+// TOP PART
 int b;
+cout << "enter the no:";
+cin >> b;
+for(int i=0; i<n; i++){
+    for(int j=0; j<n-i-1; j++){
+        cout << " ";
+    }
+   cout << "*";
+   if(i != 0)
+   {  
+    for(int j=0; j<2*i-1; j++){
+        cout << " ";
+    }
+    cout << "*";
 
+   }
+   cout << endl;
+}
 
 
        
