35af4d9e3b076f39b76747f3a1cd5d0e56406518
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-23 11:04:08 +0530

    rev

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -9,12 +9,24 @@ int linearsearch(int array[], int n){
 }
 return -1;
 }
+void revarray(int arry[]){
+    int start = 0, end = 4;
+while(start < end){
+    swap(arry[start], arry[end]);
+    start++; end--;
+}
+}
 int main(){
 int arr[] = {0, 2, 4, 8, 6, 9};
 cout << linearsearch(arr, 6) << endl;
+// reverse an array
+int arr1[] = {2, 8, 6, 4, 3};
+revarray(arr1);
+for(int i=0; i<5; i++){ 
+cout << arr1[i] << " ";
 
-
-
+}
+cout << endl;
 
 
     return 0;
