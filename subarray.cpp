fb060d95e4d3b70a995696ded77072c14a7db4b7
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-23 21:52:51 +0530

    s

--- subarray.cpp
+++ subarray.cpp
@@ -9,9 +9,29 @@ int main(){
             cout << arr[i] ;
         } cout << " " ;
     } cout << endl;
- }
+ } 
 
+int a[] = {3, -4, 5, 4, -1, 7, -8};
+int maxsum = INT8_MIN;
+ for(int st =0; st<7; st++){
+    int currsum = 0;
+    for(int end =st; end <7; end++){
+        currsum += a[end];
 
+        maxsum = max(maxsum, currsum);}}
+        cout << maxsum;
+    
+cout << endl;
+int b[] = {3, -4, 5, 4, -1, 7, -8};
+int cs = 0; // current sum
+int ms = INT8_MIN; // max sum 
+for(int i=0; i<7; i++){
+    cs += b[i];
+    ms = max(ms, cs);
+    if(cs < 0){
+        cs = 0;
+    }
+} cout << ms;
 
 
 
