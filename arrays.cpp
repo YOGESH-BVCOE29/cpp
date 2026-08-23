e104a4def47007debf11e5bc79d5d4df3c8e131d
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-08-23 10:14:35 +0530

    d

--- arrays.cpp
+++ arrays.cpp
@@ -12,5 +12,24 @@ cout << marks[0] << endl;
 for(int i=0; i<5; i++){
     cout << marks[i] << endl;
 }
+// inputting an array
+int price[5];
+for(int i=0; i<5; i++){
+    cin >> price[i];
+}
+for(int i=0; i<5; i++){
+    cout << price[i] << endl;
+}
+// finding smallest and largest no of an array
+int arr[] = {5, 12, 22, 1, 15, 24};
+int smallest = INT8_MAX;
+for(int i=0; i<5; i++){
+    if(arr[i] < smallest){
+        smallest = arr[i];
+    } 
+}cout << smallest << endl;
+
+
+
 return 0;
 }
