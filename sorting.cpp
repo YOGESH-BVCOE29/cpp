fa2a466cadf7f881bf331eaa68563b80b88cd450
YOGESH-BVCOE29 <yogeshsharma999011@gmail.com>
2026-09-12 13:44:35 +0530

    w

--- sorting.cpp
+++ sorting.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+// bubble sort
 void bubblesort(int arr[], int n){
     for(int i=0; i<n-1; i++){
         for(int j=0; j<n-i-1; j++){
@@ -7,6 +8,17 @@ void bubblesort(int arr[], int n){
                 swap(arr[j], arr[j+1]);
             }
         }
+    }
+}
+// selection sort 
+void selectionsort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        int smallidx = i;
+        for(int j=i+1; j<n; j++){
+         if(arr[j] < arr[smallidx]){
+            j = smallidx;
+         }
+        } swap(arr[i], arr[smallidx]);
     }
 }
 void printarr(int arr[], int n){
@@ -19,5 +31,7 @@ int main(){
   int arr[] = {4, 1, 5, 2, 3};
    bubblesort(arr, n);
 printarr(arr, n);
-    return 0;
+selectionsort(arr, n);
+printarr(arr, n);
+     return 0;
 }
